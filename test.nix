d1f90c4b96b0be820f8d60bf43c0bdc72b2dd87f
Patrick Wozniak <email@patwoz.de>
2025-02-08 21:54:45 +0100

    init

---
+++ test.nix
@@ -0,0 +1,30 @@
+# let
+#   a = { b = { c = 50; }; };
+# in
+# rec {
+#   h = a.b.c + 5;
+#   x.y.z = h + 5;
+# }
+
+let
+  x = 1;
+  y = 2;
+  z = { a = 1; b = 2; };
+  inherit ({ f = 1; g = 1; }) f g;
+  name = "Patrick";
+  # tt = 1;
+  out = "Nix";
+in
+{
+  inherit x y;
+  inherit (z) a;
+  f = f;
+  greets = "Hello ${name}";
+  # bla = "Hey ${tt}";
+  ee = "echo ${out} > $out";
+  cool = ''
+  Nice!
+  Second line
+  '';
+  test = <nixpkgs/lib>;
+}
