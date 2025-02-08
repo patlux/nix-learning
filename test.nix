61c307193b7f323691a072e4896a002ffaf26c80
Patrick Wozniak <email@patwoz.de>
2025-02-08 22:54:09 +0100

    functions

--- test.nix
+++ test.nix
@@ -6,25 +6,35 @@
 #   x.y.z = h + 5;
 # }
 
+# let
+#   x = 1;
+#   y = 2;
+#   z = { a = 1; b = 2; };
+#   inherit ({ f = 1; g = 1; }) f g;
+#   name = "Patrick";
+#   # tt = 1;
+#   out = "Nix";
+# in
+# {
+#   inherit x y;
+#   inherit (z) a;
+#   f = f;
+#   greets = "Hello ${name}";
+#   # bla = "Hey ${tt}";
+#   ee = "echo ${out} > $out";
+#   cool = ''
+#   Nice!
+#   Second line
+#   '';
+#   test = <nixpkgs/lib>;
+# }
+
+# functions
+
+# x: x + 1
+
 let
-  x = 1;
-  y = 2;
-  z = { a = 1; b = 2; };
-  inherit ({ f = 1; g = 1; }) f g;
-  name = "Patrick";
-  # tt = 1;
-  out = "Nix";
+  # add = x: x + 1;
+  add = { x, y }: x + y;
 in
-{
-  inherit x y;
-  inherit (z) a;
-  f = f;
-  greets = "Hello ${name}";
-  # bla = "Hey ${tt}";
-  ee = "echo ${out} > $out";
-  cool = ''
-  Nice!
-  Second line
-  '';
-  test = <nixpkgs/lib>;
-}
+  [ (add { x = 1; y = 2; }) ]
