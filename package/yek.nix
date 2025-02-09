5c059e6a82c977b928db3bbb8c56700c38e62da6
Patrick Wozniak <email@patwoz.de>
2025-02-09 19:13:42 +0100

    enable useFetchCargoVendor

--- package/yek.nix
+++ package/yek.nix
@@ -17,7 +17,8 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-5FWjjc7JmEhBjbIy/8A7IGwyC4Rjp581zT+CMUd4XBI=";
   };
 
-  cargoHash = "sha256-GNmSlWyZe2j1OXmKEzCHrSwqBCyJfXoXhkFP/vcK+Qc=";
+  cargoHash = "sha256-Glqzsw3N9ccMyFG41VfsPgp+K1+1S2DgUAqTot1TBw4=";
+  useFetchCargoVendor = true;
 
   nativeBuildInputs = [perl git];
   buildNoDefaultFeatures = true;
