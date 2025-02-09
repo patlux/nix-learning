cc1bf0c0bbc43cd9b951e1f6bee26e0b872912a3
Patrick Wozniak <email@patwoz.de>
2025-02-09 12:37:27 +0100

    build for all systems

--- package/flake.nix
+++ package/flake.nix
@@ -1,16 +1,17 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, ... }: let 
-    system = "aarch64-darwin";
-  in {
-    packages."${system}".default = derivation {
-      inherit system;
-      name = "yek";
-      builder = "${nixpkgs.legacyPackages."${system}".bash}/bin/bash";
-      args = [ "-c" "echo foo > $out" ];
-    };
-  };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default = derivation {
+        inherit system;
+        name = "yek";
+        builder = "${nixpkgs.legacyPackages."${system}".bash}/bin/bash";
+        args = [ "-c" "echo foo > $out" ];
+      };
+    });
+  
 }
