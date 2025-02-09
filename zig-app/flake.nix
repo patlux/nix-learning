9351d83664b4cf8a46609cb5a1681088dffaa11b
Patrick Wozniak <email@patwoz.de>
2025-02-09 22:19:12 +0100

    comment without flake-utils

--- zig-app/flake.nix
+++ zig-app/flake.nix
@@ -4,6 +4,21 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
+  # outputs = { nixpkgs, ... }: let
+  #   system = "aarch64-darwin";
+  #   pkgs = nixpkgs.legacyPackages.${system};
+  # in with pkgs; {
+  #   packages."${system}".default = stdenv.mkDerivation {
+  #     name = "zig-app";
+  #     version = "0.0.0";
+  #     src = ./.;
+  #     # Otherwis we get "error: ReadOnlyFileSystem" 
+  #     ZIG_GLOBAL_CACHE_DIR = "$(mktemp -d)";
+  #     buildPhase = "zig build -p $out";
+  #     nativeBuildInputs = [ zig ];
+  #   };
+  # };
+
   outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
     let 
