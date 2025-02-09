23d32b68feaa623e193b8ba94f30583a95ebe2b4
Patrick Wozniak <email@patwoz.de>
2025-02-09 12:27:57 +0100

    nix build

--- package/flake.nix
+++ package/flake.nix
@@ -2,4 +2,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+
+  outputs = { nixpkgs, ... }: let 
+    system = "aarch64-darwin";
+  in {
+    packages."${system}".default = derivation {
+      inherit system;
+      name = "yek";
+      builder = "${nixpkgs.legacyPackages."${system}".bash}/bin/bash";
+      args = [ "-c" "echo foo > $out" ];
+    };
+  };
 }
